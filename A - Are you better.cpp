fcd943f54990949b1f00613b4f20dda25942d169
Md. Jubayer Ahmad <mdjubayerahmad16@gmail.com>
2024-12-03 17:54:42 +0600

    Add files via upload

---
+++ A - Are you better.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int X;
+    cin >> X;
+
+    if( X <= 60)
+    {
+        cout << "YES";
+    }
+    else if(60 < X <= 100)
+    {
+        cout << "NO";
+    }
+
+
+    return 0;
+}
